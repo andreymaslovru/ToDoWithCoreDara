6cdbe5bf7ddae42fb50ae2343ef048f102ad5be6
Andrey Maslov <andrey.ma2001@gmail.com>
2021-10-12 10:59:11 +0700

    added the ability to edit the note

--- ToDoWithCoreDara/ViewController.swift
+++ ToDoWithCoreDara/ViewController.swift
@@ -57,6 +57,34 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let item = models[indexPath.row]
+        
+        let sheet = UIAlertController(title: "Edit", message: "Edit a task", preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+            let alert = UIAlertController(title: "Edit", message: "Edit a task", preferredStyle: .alert)
+            
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else { return }
+                self?.updateItem(item: item, newName: newName)
+            }))
+            
+            self.present(alert, animated: true)
+        }))
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { _ in
+            self.deleteItem(item: self.models[indexPath.row])
+        }))
+        
+        present(sheet, animated: true)
+    }
+    
     
     //Core Data
     
@@ -100,6 +128,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         do {
             try context.save()
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         } catch let error {
             print(error)
         }
